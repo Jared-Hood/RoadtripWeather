c9ead8a717e73ddcb6a2366ba04b490aa5982d35
jaredhood <jwh6ry@virginia.edu>
2019-03-02 17:25:25 -0500

    gui

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ import googleImages
 import roadMap
 import getWeather
 
-start = "Paris"
-end = "Berlin"
+start = "Charlottesville VA"
+end = "Texas"
 days = 5
 
 directions = roadMap.getDirections(start,end)
 parsed_directions = roadMap.parseWaypoints(directions,days)
 city_state, full_address = roadMap.reverseGeo(parsed_directions[1])
 
-iter = 0
+iter = 1
 for i in range(len(parsed_directions)):
     lat = str(parsed_directions[i][0])
     long = str(parsed_directions[i][1])
-    currentWeather = getWeather.getWeather(lat, long)[1][2]
+    currentWeather = getWeather.getWeather(lat, long)[iter][2]
 
-    city_state, full_address = roadMap.reverseGeo(parsed_directions[iter])
+    city_state, full_address = roadMap.reverseGeo(parsed_directions[i])
     googleImages.getPics(city_state + " " + currentWeather)
     iter += 1
 
     print(currentWeather + ": " + city_state)
-
